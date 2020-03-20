78b3d25093355c1cd47450b2fe149d63a8ec8548
nate <nate@thinkundergound.com>
2020-03-20 15:56:19 -0400

    added blockquote style for rendered markdown

--- src/components/generic_content.js
+++ src/components/generic_content.js
@@ -2,6 +2,20 @@ import React from 'react'
 import SbEditable from 'storyblok-react'
 const ReactMarkdown = require('react-markdown')
 
+const style = {
+  borderLeft: '4px solid rgb(223, 226, 229)',
+  padding: '0 16px',
+  marginBottom: 16,
+}
+
+
+function BlockQuoteRenderer({ children }) {
+  return (
+      <blockquote style={style}>
+        {children}
+      </blockquote>
+  )
+}
 
 const GenericContent = (props) => {
 	return (
@@ -9,7 +23,7 @@ const GenericContent = (props) => {
 	    <div className="container pt-4">
 		<div className="row">
 	    	<div className="col-12">
-		<ReactMarkdown source={props.blok.content} />
+		<ReactMarkdown source={props.blok.content} renderers={{'blockquote': BlockQuoteRenderer}}/>
 	    	</div>
 		</div>
 	      </div>
