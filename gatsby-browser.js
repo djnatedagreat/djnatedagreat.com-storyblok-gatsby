3e45093dc2cc92e7f5f882a9ad05f0decebe57e3
Nate Frank <nate@greatdayatwork.com>
2021-09-10 11:35:32 -0400

    Core Web Vital Performance Improvements: Triming back javascript includes

--- gatsby-browser.js
+++ gatsby-browser.js
@@ -6,6 +6,10 @@
 
 // You can delete this file if you're not using it
 import './src/bootstrap/custom.scss'
-import 'jquery/dist/jquery.min.js'
-import 'popper.js/dist/popper.min'
-import 'bootstrap/dist/js/bootstrap.min.js'
+//import 'jquery/dist/jquery.min.js'
+//import 'popper.js/dist/popper.min'
+import 'bootstrap/js/dist/util.js'
+import 'bootstrap/js/dist/modal.js'
+import 'bootstrap/js/dist/dropdown.js'
+import 'bootstrap/js/dist/collapse.js'
+//import 'bootstrap/dist/js/bootstrap.min.js'
